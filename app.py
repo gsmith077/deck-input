894b372a988ffc7ea2f265ac4dcf29b978d651c0
Gabriel Smith <33425559+gsmith077@users.noreply.github.com>
2024-03-17 00:52:03 -0400

    A good ol' fashioned refactor

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+Launches a flask app that takes inputs and recreates keystrokes based on those 
+inputs.
+"""
+
 import json
 import yaml
 
@@ -7,59 +12,96 @@ from pynput.keyboard import Controller, Key
 app = Flask(__name__)
 virtual_keyboard = Controller()
 
+# Set a group of recognized "modifier" keys for filtering.
+MODIFIERS = ["shift", "alt", "control", "cmd"]
+
+
+def standardize_long_form(button: dict) -> dict:
+    """
+    Sanitizes the inputs of the long form to ensure it's the correct format.
+    """
+    if isinstance(button["inputs"], str):
+        button["inputs"] = [button["inputs"]]
+    return button
+
+
+def standardize_short_form(button: dict) -> dict:
+    """
+    Sanitizes the inputs of the short form to ensure it's the correct format.
+    """
+    for key, value in button.items():
+        standardized_button = {
+            "label": key,
+            "inputs": [value] if isinstance(value, str) else value,
+        }
+    return standardized_button
+
 
 def format_buttons(buttons: dict) -> dict:
+    """
+    Determines if the button is long form (based on presence of `inputs` key) or
+    short term (lacks that key).
+    Sends the button for standardization, and returns all formatted buttons.
+    """
     formatted_buttons = []
 
     for button in buttons:
         if "inputs" in button:
-            button["inputs"] = (
-                [button["inputs"]]
-                if isinstance(button["inputs"], str)
-                else button["inputs"]
-            )
+            standardized_button = standardize_long_form(button)
         else:
-            button = [
-                {
-                    "label": key,
-                    "inputs": [value] if isinstance(value, str) else value,
-                }
-                for key, value in button.items()
-            ][0]
-
-        formatted_buttons.append(button)
+            standardized_button = standardize_short_form(button)
+        formatted_buttons.append(standardized_button)
     return formatted_buttons
 
 
 @app.route("/")
 def main():
+    """
+    Default route of the web app. Parses the YAML, sends it for formatting, and
+    returns the rendered template.
+    """
     with open("layout.yaml", "r") as layout_file:
         layout = yaml.safe_load(layout_file)
 
     buttons = format_buttons(layout["buttons"])
-    print(buttons)
-    return render_template("index.html", buttons=buttons)
+
+    return render_template(
+        "index.html",
+        buttons=buttons,
+    )
+
+
+def prepare_keys(inputs: dict) -> dict:
+    """
+    This function handles two things:
+     - Strips the modifiers from the list.
+     - Casts keys into `Keys.{}` objects, for handling F keys and
+       multi-character named keys (like backspace)
+    """
+    return [
+        getattr(Key, current_key, current_key)
+        for current_key in inputs
+        if current_key not in MODIFIERS
+    ]
 
 
 @app.route("/inputs", methods=["POST"])
 def perform_input():
-    modifiers = ["shift", "alt", "control", "cmd"]
+    """
+    This is the main function. Parses the incoming request for `inputs` and
+    turns them into `pynput` calls.
+    """
 
     req_data = json.loads(request.data)
 
     inputs = req_data["inputs"]
 
-    mods = [getattr(Key, mod) for mod in inputs if mod in modifiers]
-    keys = [key for key in inputs if key not in modifiers]
-    keys = [getattr(Key, current_key, current_key) for current_key in keys]
-
-    print("Mods: %", mods)
-    print("Keys: %", keys)
+    mods = [getattr(Key, mod) for mod in inputs if mod in MODIFIERS]
+    keys = prepare_keys(inputs)
 
     if len(keys) == 1:
         with virtual_keyboard.pressed(*mods):
-            virtual_keyboard.press(keys[0])
-            virtual_keyboard.release(keys[0])
+            virtual_keyboard.tap(keys[0])
     else:
         print("Currently only support mods and 1 key.")
 
